0ed26b4d5e62292d58d503dd7553bf638b1a8538
Rick Henry <rickhenry@rickhenry.dev>
2023-09-04 20:02:57 -0400

    fix lighthouse issues
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 
-const common = {
-  url: process.env.URL,
-};
+const common = { };
 
 const testing = {
   ...common,
+  url: "http://localhost:8080",
 };
 
 const production = {
   ...common,
+  url: "https://tabletennisscoreboard.com",
 };
 
 module.exports = process.env.ELEVENTY_PRODUCTION ? production : testing;
